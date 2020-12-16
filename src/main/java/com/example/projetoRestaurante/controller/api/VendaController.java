b934f616cc33ff7d22ed84715c7d324b156faef2
Vinicius Bastos <43853758+vinicius-hora@users.noreply.github.com>
2020-12-16 20:59:01 -0300

    Correção V1.1
    Renomear APICONTROLLER

--- src/main/java/com/example/projetoRestaurante/controller/api/VendaController.java
+++ src/main/java/com/example/projetoRestaurante/controller/api/VendaController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.projetoRestaurante.model.Estoque;
 import com.example.projetoRestaurante.model.Venda;
 import com.example.projetoRestaurante.service.VendaService;
 
